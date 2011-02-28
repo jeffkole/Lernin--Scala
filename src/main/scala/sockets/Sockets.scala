9f387b9789f1c04d7d9e26c9c63c87a8e0bc8d99
Jeff Kolesky <jeff@kolesky.com>
2011-02-28 08:16:03 -0500

    adds a socket server and client

---
+++ src/main/scala/sockets/Sockets.scala
@@ -0,0 +1,83 @@
+package com.kolesky.sockets
+
+import java.io._
+import java.net._
+
+class EchoServer(port: Int) {
+  def run: Unit = {
+    val server = new ServerSocket(port)
+    while (true) {
+      val socket = server.accept()
+      val input = new DataInputStream(new BufferedInputStream(socket.getInputStream))
+      val output = new DataOutputStream(new BufferedOutputStream(socket.getOutputStream))
+
+      val contents = new ByteArrayOutputStream
+
+      val buffer = new Array[Byte](8 * 1024)
+      var read = -1
+      do {
+        read = input.read(buffer)
+        printf("read %d bytes: '%s'%n", read, new String(buffer))
+        contents.write(buffer, 0, read)
+        printf("writing '%s'%n", new String(buffer))
+        output.write(buffer, 0, read)
+        output.flush
+        if (read < buffer.size) { read = -1 }
+      } while (read > 0)
+
+      contents.close
+      printf("read in total '%s'%n", contents.toString)
+
+      output.close
+      input.close
+      socket.close
+    }
+  }
+}
+
+object EchoServer {
+  def main(args: Array[String]) {
+    (new EchoServer(11111)).run
+  }
+}
+
+class EchoClient(hostname: String, port: Int) {
+  def run: Unit = {
+    while (true) {
+      printf("> ")
+      val echo = readLine
+      printf("read from line '%s'%n", echo)
+
+      val socket = new Socket(hostname, port)
+      val input = new DataInputStream(new BufferedInputStream(socket.getInputStream))
+      val output = new DataOutputStream(new BufferedOutputStream(socket.getOutputStream))
+      output.writeChars(echo)
+      output.flush
+
+      val contents = new ByteArrayOutputStream
+
+      val buffer = new Array[Byte](8 * 1024)
+      var read = -1
+      do {
+        printf("reading...%n")
+        read = input.read(buffer)
+        printf("read %d bytes: '%s'%n", read, new String(buffer))
+        contents.write(buffer, 0, read)
+        if (read < buffer.size) { read = -1 }
+      } while (read > 0)
+
+      contents.close
+      printf("ECHO: %s%n", contents.toString)
+
+      output.close
+      input.close
+      socket.close
+    }
+  }
+}
+
+object EchoClient {
+  def main(args: Array[String]) {
+    (new EchoClient("localhost", 11111)).run
+  }
+}
