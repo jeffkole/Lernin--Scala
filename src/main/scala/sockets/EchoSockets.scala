f173793ad04affc2ce8506a8ebe398c485756839
Jeff Kolesky <jeff@kolesky.com>
2011-03-12 16:06:42 -0500

    simplifies the design of the sockets

--- src/main/scala/sockets/EchoSockets.scala
+++ src/main/scala/sockets/EchoSockets.scala
@@ -1,38 +1,20 @@
 package com.kolesky.sockets
 
-import java.io._
 import java.net.ServerSocket
+import java.net.Socket
 
 class EchoServer(port: Int) {
   def run: Unit = {
     val server = new ServerSocket(port)
     while (true) {
-      val socket = server.accept()
-      val input = new DataInputStream(new BufferedInputStream(socket.getInputStream))
-      val output = new DataOutputStream(new BufferedOutputStream(socket.getOutputStream))
-
       var run = true
+      val socket = new SimpleSocket(server.accept(), 0x0)
       while (run) {
-        val contents = new ByteArrayOutputStream
-        val buffer = new Array[Byte](8 * 1024)
-        var read = -1
-        do {
-          read = input.read(buffer)
-          if (read > 0) {
-            printf("[S] read %d bytes: '%s'%n", read, new String(buffer))
-            contents.write(buffer, 0, read)
-            printf("[S] writing '%s'%n", new String(buffer))
-            output.write(buffer, 0, read)
-            output.flush
-          }
-        } while (read > 0 && read >= buffer.size)
-        contents.close
-        printf("[S] read in total '%s'%n", contents.toString)
-        if (read <= 0) { run = false }
+        val msg = socket.receive
+        printf("[S] received '%s'%n", msg)
+        socket.send(msg)
+        if (msg.length <= 0) { run = false }
       }
-
-      output.close
-      input.close
       socket.close
     }
   }
@@ -46,13 +28,13 @@ object EchoServer {
 
 class EchoClient(hostname: String, port: Int) {
   def run: Unit = {
-    val socket = new StringClientSocket(hostname, port, 0x0)
+    val socket = new StringSocket(new Socket(hostname, port), 0x0)
     while (true) {
       printf("> ")
       val echo = readLine
       printf("[C] read from line '%s'%n", echo)
 
-      socket.send(echo.asInstanceOf[socket.T])
+      socket.send(echo)
       printf("[C] ECHO: %s%n", socket.receive)
     }
     socket.close
@@ -60,8 +42,8 @@ class EchoClient(hostname: String, port: Int) {
 
   // Really just meant to run some tests
   def runOnce(msg: String): String = {
-    val socket = new StringClientSocket(hostname, port, 0x0)
-    socket.send(msg.asInstanceOf[socket.T])
+    val socket = new StringSocket(new Socket(hostname, port), 0x0)
+    socket.send(msg)
     val response = socket.receive
     socket.close
     response.asInstanceOf[String]
